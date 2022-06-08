c1f76baa12c8c04b256b589297a0d46fed1930cd
yuvraj <yuvrajhere25@gmail.com>
2022-06-08 11:38:43 +0530

    Made a banner on home

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,9 +1,21 @@
 // components
+import Banner from "./Banner";
 import NavBar from "./NavBar";
+import {Box, styled} from '@mui/material';
+
+const Component = styled(Box)`
+  padding: 10px 10px;
+  background: #F2F2F2;
+`
 
 const Home = () => {
   return (
-    <NavBar />
+    <>
+      <NavBar />
+      <Component>
+        <Banner />
+      </Component>
+    </>
   )
 }
 
