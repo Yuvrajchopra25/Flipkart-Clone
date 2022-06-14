d2e16d04d793b0ff23ac42ec9cdf8ee876c61549
yuvraj <yuvrajhere25@gmail.com>
2022-06-14 14:24:21 +0530

    Making schema for products

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import Connection from "./database/db.js";
+import DefaultData from './default.js';
 
 const app = express();
 
@@ -15,3 +16,5 @@ const PASSWORD = process.env.DB_PASSWORD;
 Connection(USERNAME, PASSWORD);
 
 app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+
+DefaultData();
